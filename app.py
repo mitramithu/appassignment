769bce6f7a02b47468f429277504bc6392d5b94a
mitramithu <103366245+mitramithu@users.noreply.github.com>
2022-04-10 23:30:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+# area of circle
+# class circle():
+#     def __init__(self,radius):
+#         self.radius=radius
+#     def perimeter(self):
+#         return 2*3.14*self.radius
+#     def area(self):
+#         return 3.14*self.radius**2
+#
+# a=circle(2)
+# print(a.area())
+# print(a.perimeter())
+
+##
+filename=input('enter your filename:')
+f=filename.split('.')
+print('the extension of the file is:' + f[-1])
