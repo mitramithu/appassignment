9ae4732b14a81d9a320580a3592be4fbd33b0e2c
mitramithu <103366245+mitramithu@users.noreply.github.com>
2022-04-10 23:58:37 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,27 @@
+
+# n=int(input('enter'))
+# n1,n2=0,1
+# count=0
+# if n<=0:
+#     print('enter positive integer:')
+# elif n==1:
+#     print('fibo series',n,':')
+#     print(n1)
+# else:
+#     print('fibo series:')
+#     while count<n:
+#         print(n1)
+#         nth=n1+n2
+#         n1=n2
+#         n2=nth
+#         count+=1
+
+##
+l1=[12,90,-9,-6,45]
+for num in l1:
+    if num>=0:
+        print(num,end=' ')
+l2=[-45,98,-8,32]
+for num in l2:
+    if num>=0:
+        print(num,end=' ')
